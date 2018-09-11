dd2709348bec287d4674afd8ceefd1a4435583a0
Steven Jimenez <stevenmjimenez@gmail.com>
2018-09-10 22:10:52 -0400

    [codacy] Update tests to pass quality checks

--- test/classification.js
+++ test/classification.js
@@ -1,27 +1,31 @@
 import test from 'ava';
 import { Classification } from '../dist/classifier';
 
-test('Classification-only has no slashes', t => {
+test('Classification-only has no slashes', (t) => {
   const classification = new Classification();
+
   t.is(classification.toString(), 'UNCLASSIFIED');
 });
 
-test('US Classification//Codewords', t => {
+test('US Classification//Codewords', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addCodeword('SI');
   t.is(classification.toString(), 'TOP SECRET//SI');
 });
 
-test('US Classification//FGI', t => {
+test('US Classification//FGI', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addFgi({ owner: 'CAN' });
   t.is(classification.toString(), 'TOP SECRET//FGI CAN');
 });
 
-test('US Classification//Codewords//FGI', t => {
+test('US Classification//Codewords//FGI', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addCodeword('SI');
   classification.addCodeword('TK');
@@ -30,16 +34,18 @@ test('US Classification//Codewords//FGI', t => {
   t.is(classification.toString(), 'TOP SECRET//SI/TK//FGI CAN GBR');
 });
 
-test('US Classification//Dissemination', t => {
+test('US Classification//Dissemination', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.setRsen(true);
   classification.addRel('CAN');
   t.is(classification.toString(), 'TOP SECRET//RSEN,REL TO USA and CAN');
 });
 
-test('US Classification//Codewords//Dissemination', t => {
+test('US Classification//Codewords//Dissemination', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addCodeword('SI');
   classification.addCodeword('TK');
@@ -48,8 +54,9 @@ test('US Classification//Codewords//Dissemination', t => {
   t.is(classification.toString(), 'TOP SECRET//SI/TK//RSEN,REL TO USA and CAN');
 });
 
-test('US Classification//FGI//Dissemination', t => {
+test('US Classification//FGI//Dissemination', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addFgi({ owner: 'CAN' });
   classification.addFgi({ owner: 'GBR' });
@@ -58,8 +65,9 @@ test('US Classification//FGI//Dissemination', t => {
   t.is(classification.toString(), 'TOP SECRET//FGI CAN GBR//RSEN,REL TO USA and CAN');
 });
 
-test('US Classification//Codewords//FGI//Dissemination', t => {
+test('US Classification//Codewords//FGI//Dissemination', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addFgi({ owner: 'CAN' });
   classification.addFgi({ owner: 'GBR' });
--- test/non-ic.js
+++ test/non-ic.js
@@ -1,22 +1,25 @@
 import test from 'ava';
 import { Classification } from '../dist/classifier';
 
-test('Non-IC markings default to blank', t => {
+test('Non-IC markings default to blank', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   t.is(classification.toString(), 'TOP SECRET');
 });
 
-test('Non-IC markings are separated with spaceless commas', t => {
+test('Non-IC markings are separated with spaceless commas', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addNonIC('comsec');
   classification.addNonIC('SAR');
   t.is(classification.toString(), 'TOP SECRET//COMSEC,SAR');
 });
 
-test('Non-IC markings do not display duplicates', t => {
+test('Non-IC markings do not display duplicates', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addNonIC('comsec');
   classification.addNonIC('comsec');
@@ -25,8 +28,9 @@ test('Non-IC markings do not display duplicates', t => {
   t.is(classification.toString(), 'TOP SECRET//COMSEC');
 });
 
-test('Non-IC markings may be removed', t => {
+test('Non-IC markings may be removed', (t) => {
   const classification = new Classification();
+
   classification.setClassificationLevel(4);
   classification.addNonIC('comsec');
   classification.remNonIC('COMSEC');
