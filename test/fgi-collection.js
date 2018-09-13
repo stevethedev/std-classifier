6a7cde5914f982fe0cf3adef0deb64253ee8042d
Steven Jimenez <stevenmjimenez@gmail.com>
2018-09-13 00:10:00 -0400

    [collections] Test the Fgi Collection count method

--- test/fgi-collection.js
+++ test/fgi-collection.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import { Classification } from '../src/main';
+import { FgiCollection } from '../src/fgi-collection';
 
 test('Collecting multiple FGIs from multiple owners and creating an array sorts by owner-name in ascending order', (t) => {
   const classification = new Classification();
@@ -28,4 +29,28 @@ test('Collecting multiple FGIs from one owner and creating an array sorts by lev
   t.is(fgis[0].level, 0);
   t.is(fgis[1].level, 2);
   t.is(fgis[2].level, 4);
+});
+
+test('The FGI Collection class reports a count based on how many valid values there are', (t) => {
+  const fgis = new FgiCollection();
+
+  t.is(fgis.count(), 0);
+
+  fgis.add({ owner: 'CAN', level: 2 });
+  t.is(fgis.count(), 1);
+
+  fgis.add({ owner: 'CAN', level: 0 });
+  t.is(fgis.count(), 2);
+
+  fgis.add({ owner: 'CAN', level: 4 });
+  t.is(fgis.count(), 3);
+
+  fgis.rem(fgis.find({ owner: 'CAN', level: 2 }));
+  t.is(fgis.count(), 2);
+
+  fgis.rem(fgis.find({ owner: 'CAN', level: 3 }));
+  t.is(fgis.count(), 2);
+
+  fgis.rem(fgis.find({ owner: 'CAN', level: 2 }));
+  t.is(fgis.count(), 2);
 });
