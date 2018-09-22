e2a6272476ff866eb73f9eb0546a8c97f24f04f5
Steven Jimenez <stevenmjimenez@gmail.com>
2018-09-21 22:40:38 -0400

    [master] Restructure build system

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from 'rollup-plugin-typescript2';
+
+import path from 'path';
+
+const rootFolder = (...filePath) => path.join(...filePath);
+const srcFolder = (...filePath) => rootFolder('src', ...filePath);
+const outFolder = (...filePath) => rootFolder('dist', ...filePath);
+
+export default {
+  input: srcFolder('main.ts'),
+  output: {
+    name: 'classification',
+    file: outFolder('classifier.js'),
+    format: 'umd',
+    globals: {
+      'date-fns': 'dateFns'
+    },
+  },
+  external: ['date-fns'],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+      tsconfigOverride: {
+        compilerOptions: {
+          module: 'es2015',
+          outDir: './dist',
+        }
+      }
+    })
+  ],
+};
